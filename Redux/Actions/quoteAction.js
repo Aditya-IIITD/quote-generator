582a494b4dbf166d05d37c26bb00e04f20726139
aditya19137 <58390543+Aditya-IIITD@users.noreply.github.com>
2024-01-05 01:38:15 +0530

    Update quoteAction.js

--- Redux/Actions/quoteAction.js
+++ Redux/Actions/quoteAction.js
@@ -3,5 +3,5 @@ export const IS_SUCCESS = "success";
 export const IS_ERROR = "error";
 
 export const isLoading = () => ({ type: IS_LOADING });
-export const isSuccess = (quote) => ({ type: IS_SUCCESS, quote: { ...quote } });
+export const isSuccess = (quote) => ({ type: IS_SUCCESS, quote: quote });
 export const isError = (err) => ({ type: IS_ERROR, error: err });
