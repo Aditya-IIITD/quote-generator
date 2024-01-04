30f40567097ef6a2fa9316145f9ec50834a9ce9c
aditya19137 <58390543+Aditya-IIITD@users.noreply.github.com>
2024-01-05 01:39:52 +0530

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import style from "../Styles/navbar.module.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <div className={style.container}>
-        <h1>Home</h1>
-        <h1 className={style.bookmark}>Bookmarks</h1>
+        <h1>
+          <Link to="/">Home</Link>
+        </h1>
+        <h1 className={style.bookmark}>
+          <Link to="bookmarks">Bookmarks</Link>
+        </h1>
       </div>
       <Outlet />
     </>
