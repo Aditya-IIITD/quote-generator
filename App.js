efedd6963b57097ecb700dba5061ffd5795b2a5d
aditya19137 <58390543+Aditya-IIITD@users.noreply.github.com>
2024-01-05 01:36:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,23 +2,36 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
+import BookMark from "./Pages/Bookmark";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Navbar />,
+      errorElement: <h1>Something Went Wrong</h1>,
       children: [
         {
           index: true,
           element: <Home />,
+        },
+        {
+          path: "bookmarks",
+          element: <BookMark />,
         },
       ],
     },
   ]);
   return (
     <>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </Provider>
     </>
   );
 }
